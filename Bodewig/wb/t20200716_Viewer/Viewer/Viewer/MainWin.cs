3b16bebc760051c9e4c0f56ee7b0309d55cc0749
stackprobe <stackprobes@gmail.com>
2020-07-17 01:04:01 +0900

    Egret

---
+++ Bodewig/wb/t20200716_Viewer/Viewer/Viewer/MainWin.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Security.Permissions;
+using System.Windows.Forms;
+using Charlotte.Tools;
+
+namespace Charlotte
+{
+	public partial class MainWin : Form
+	{
+		#region ALT_F4 抑止
+
+		private bool XPressed = false;
+
+		[SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.UnmanagedCode)]
+		protected override void WndProc(ref Message m)
+		{
+			const int WM_SYSCOMMAND = 0x112;
+			const long SC_CLOSE = 0xF060L;
+
+			if (m.Msg == WM_SYSCOMMAND && (m.WParam.ToInt64() & 0xFFF0L) == SC_CLOSE)
+			{
+				this.XPressed = true;
+				return;
+			}
+			base.WndProc(ref m);
+		}
+
+		#endregion
+
+		public MainWin()
+		{
+			InitializeComponent();
+		}
+
+		private void MainWin_Load(object sender, EventArgs e)
+		{
+			// noop
+		}
+
+		private void MainWin_Shown(object sender, EventArgs e)
+		{
+			// -- 0001
+
+			// ----
+
+			this.MTBusy.Leave();
+		}
+
+		private void MainWin_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			// noop
+		}
+
+		private void MainWin_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			this.MTBusy.Enter(); // 2bs
+
+			// ----
+
+			// -- 9999
+		}
+
+		private void CloseWindow()
+		{
+			using (this.MTBusy.Section())
+			{
+				try
+				{
+					// -- 9000
+
+					// ----
+				}
+				catch (Exception e)
+				{
+					MessageBox.Show("" + e, "Error @ CloseWindow()", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
+				this.MTBusy.Enter();
+				this.Close();
+			}
+		}
+
+		private VisitorCounter MTBusy = new VisitorCounter(1);
+		private long MTCount;
+
+		private void MainTimer_Tick(object sender, EventArgs e)
+		{
+			if (this.MTBusy.HasVisitor())
+				return;
+
+			this.MTBusy.Enter();
+			try
+			{
+				// -- 3001
+
+				if (this.XPressed)
+				{
+					this.XPressed = false;
+					this.CloseWindow();
+					return;
+				}
+				if (this.MTCount == 150) // 15 sec
+				{
+					this.CloseWindow();
+					return;
+				}
+			}
+			catch (Exception ex)
+			{
+				ProcMain.WriteLog(ex);
+			}
+			finally
+			{
+				this.MTBusy.Leave();
+				this.MTCount++;
+			}
+		}
+	}
+}
---
+++ Bodewig/wb/t20200716_Viewer/Viewer/Viewer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Charlotte.Tools;
+
+namespace Charlotte
+{
+	static class Program
+	{
+		/// <summary>
+		/// アプリケーションのメイン エントリ ポイントです。
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			ProcMain.GUIMain(() => new MainWin(), APP_IDENT, APP_TITLE);
+		}
+
+		public const string APP_IDENT = "{b563e8ad-ae84-4ca8-8e6f-a792b3c3c940}";
+		public const string APP_TITLE = "Viewer";
+	}
+}
