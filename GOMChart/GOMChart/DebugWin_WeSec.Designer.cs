5bc065284356cc3ee89cae9cc684295407fb6842
stackprobe <stackprobes@gmail.com>
2020-08-21 10:02:04 +0900

    Juliet

--- GOMChart/GOMChart/DebugWin_WeSec.Designer.cs
+++ GOMChart/GOMChart/DebugWin_WeSec.Designer.cs
@@ -44,7 +44,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.ClientSize = new System.Drawing.Size(284, 261);
 			this.Controls.Add(this.lblOutput);
 			this.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
 			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
